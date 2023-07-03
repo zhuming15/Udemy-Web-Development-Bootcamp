2a39f9740511dc679c3eeebeabb408d9e45f5b9b
zhuming15 <e0735464@u.nus.edu>
2023-07-03 12:31:44 +0800

    Complete example exercise

--- 19_jQuery/19.1_jQuery_Trial/index.js
+++ 19_jQuery/19.1_jQuery_Trial/index.js
@@ -1,1 +1,8 @@
-$("h1").css("color", "red");
+$(document).keypress(function (event) {
+  $("h1").addClass("big-title");
+  $("h1").text(event.key);
+});
+
+$("h1").on("click", function () {
+  $("h1").css("color", "purple");
+});
